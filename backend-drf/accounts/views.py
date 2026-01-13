e42b96bbef2c3427b1832f7597716d7970fc2373
manjeet kumar <kumarmanjeet50728@gmail.com>
2026-01-13 23:45:45 +0530

    CSRF exempt Register API for React frontend

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -2,24 +2,24 @@ from django.shortcuts import render
 from .serializers import UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
-# Create your views here.
-
+@method_decorator(csrf_exempt, name='dispatch')
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
+
 class Protected_view(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        response = {
-            'status' : "request was permitted"
-        }
-        return Response(response)
+        return Response({
+            'status': "request was permitted"
+        })
